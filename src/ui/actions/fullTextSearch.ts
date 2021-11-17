7631ab450542b57cc52cd22f81c18fcd5bc6a5e9
Colby Rabideau <478109+colbyr@users.noreply.github.com>
2021-11-17 11:41:58 -0500

    implement the search protocol

--- src/ui/actions/fullTextSearch.ts
+++ src/ui/actions/fullTextSearch.ts
@@ -1,5 +1,6 @@
+import { SearchSourceContentsMatch } from "@recordreplay/protocol";
 import { uniqueId } from "lodash";
-import { sendMessage } from "protocol/socket";
+import { client, sendMessage } from "protocol/socket";
 import { ThreadFront } from "protocol/thread";
 import { getSessionId } from "ui/reducers/app";
 import { query } from "ui/utils/apolloClient";
@@ -13,32 +14,24 @@ export type FullTextSearchAction =
   | {
       type: "FULL_TEXT_SEARCH_MATCHES";
       query: string;
-      matches: any[];
+      matches: SearchSourceContentsMatch[];
     };
 
 export function startFullTextSearch(query: string): UIThunkAction {
-  return async ({ dispatch }) => {
+  return async ({ dispatch, getState }) => {
     dispatch({ type: "FULL_TEXT_SEARCH_STARTED", query });
     console.info("search", { query });
 
-    const search: UIThunkAction = async ({ dispatch, getState }) => {
-      console.info("maybe do search");
-      const sessionId = getSessionId(getState());
+    console.info("maybe do search");
 
-      const handleMatches = result => {
-        console.info("MATCHES", result);
-        dispatch({
-          type: "FULL_TEXT_SEARCH_MATCHES",
-          query,
-          matches: result,
-        });
-      };
-      addEventListener("Debugger.searchSourceContentsMatches", handleMatches);
-      await sendMessage("Debugger.searchSourceContents", { searchId: query, query }, sessionId);
-      console.info("DONE");
-      removeEventListener("Debugger.searchSourceContentsMatches", handleMatches);
-    };
-
-    return search;
+    await ThreadFront.searchSources({ query }, matches => {
+      console.info("MATCHES", matches);
+      dispatch({
+        type: "FULL_TEXT_SEARCH_MATCHES",
+        query,
+        matches,
+      });
+    });
+    console.info("DONE");
   };
 }
